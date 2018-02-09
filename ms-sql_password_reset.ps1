607a31d85a3f59501a6d50133c4947cc6888e9e7
Vodyanoy17 <gvinopal@hotmail.com>
2018-02-09 20:17:13 +0200

    fix bug 3

--- ms-sql_password_reset.ps1
+++ ms-sql_password_reset.ps1
@@ -1,25 +1,2 @@
 function ResetPassword () {
-    <#
-    .SYNOPSIS:
-    Adds a file name extension to a supplied name.
-    .DESCRIPTION
-    Adds a file name extension to a supplied name.
-    Takes any strings for the file name or extension.
-    .PARAMETER Name
-    Specifies the file name.
-    .PARAMETER Extension
-    Specifies the extension. "Txt" is the default.
-    .INPUTS
-    None. You cannot pipe objects to Add-Extension.
-    .OUTPUTS
-    System.String. Add-Extension returns a string with the extension
-    or file name.
-    .EXAMPLE
-    C:\PS> extension -name "File"
-    File.txt
-    .LINK
-    http://www.fabrikam.com/extension.html
-    #>
-
-    # fix the bug 2
-}
+    
