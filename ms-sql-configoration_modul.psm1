c09732f34b9ed5b61a44d9e753c0d41ecdf3cf4a
Vodyanoy17 <gvinopal@hotmail.com>
2018-02-09 20:12:03 +0200

    integ commit

--- ms-sql-configoration_modul.psm1
+++ ms-sql-configoration_modul.psm1
@@ -1,24 +1,3 @@
 function MS-SQlConfiguration () {
-    <#
-    .SYNOPSIS:
-    Adds a file name extension to a supplied name.
-    .DESCRIPTION
-    Adds a file name extension to a supplied name.
-    Takes any strings for the file name or extension.
-    .PARAMETER Name
-    Specifies the file name.
-    .PARAMETER Extension
-    Specifies the extension. "Txt" is the default.
-    .INPUTS
-    None. You cannot pipe objects to Add-Extension.
-    .OUTPUTS
-    System.String. Add-Extension returns a string with the extension
-    or file name.
-    .EXAMPLE
-    C:\PS> extension -name "File"
-    File.txt
-    .LINK
-    http://www.fabrikam.com/extension.html
-    #>
-    
+        
 }
